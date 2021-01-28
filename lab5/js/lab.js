eed1f6a11f99bea4835246de838c0d9cb7c21443
jhanshaw-lab <73619707+jhanshaw-lab@users.noreply.github.com>
2021-01-27 20:33:30 -0800

    changes for lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Jacob Hanshaw
+ * Created:   01.27.2021
+ **/
+
+// Define Varibale for Car
+var carMake = "Ford";
+var carModel = "Mustang GT";
+var carColor = "Black";
+var carYear = 2006;
+
+//Calculate car age
+var carAge = 2021 - carYear;
+
+//Boolean for own it
+var ownIt = true
+
+//Output
+document.writeln("Car Make: " + carMake + "<br>");
+document.writeln("Car Model: " + carModel + "<br>");
+document.writeln("Car Color: " + carColor + "<br>");
+document.writeln("Car Year: " + carYear + "<br>");
+document.writeln("Car Age: " + carAge + "<br>");
+document.writeln("Own it?: " + ownIt + "<br>");
